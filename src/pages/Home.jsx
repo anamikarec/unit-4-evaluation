5ffc09eb4626a649645166e948c1821dc5410c8a
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2022-01-05 21:19:25 +0530

    Some more changes in files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,14 @@ import { useSelector } from "react-redux";
 import Login from "./Login";
 import { useState, useEffect } from "react";
 
-import { styled } from "@mui/material/styles";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
+import { experimentalStyled as styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(2),
   textAlign: "center",
   color: theme.palette.text.secondary
 }));
@@ -17,18 +17,13 @@ const Item = styled(Paper)(({ theme }) => ({
 const UserCard = ({ id, name, url, avatar }) => {
   return (
     <div>
-      {/* //     display: "flex",
-    //     gap: "2rem",
-    //     border: "1px solid black",
-    //     padding: "1rem"
-    //   }}
-    // >
-    //   <img width="50" src={avatar} alt={id} />
-    //   <div>{name}</div>
-    //   <div>{url}</div> */}
-      <Box sx={{ width: "100%" }}>
-        <Grid container rowSpacing={3} columnSpacing={{ xs: 3, sm: 3, md: 10 }}>
-          <Grid item xs={6}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          <Grid item xs={2} sm={4} md={4}>
             <img width="50" src={avatar} alt={id} />
             <Item>{name}</Item>
           </Grid>
